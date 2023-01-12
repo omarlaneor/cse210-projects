36fa9fdda31dad59cacf2e546e9411e356a3942e
omarlaneor <annielaneor@gmail.com>
2023-01-12 01:56:37 -0500

    activities done

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,31 @@
+
 using System;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        int randomNumber = randomGenerator.Next(1, 101);
+        int guess = -1;
+        while (guess != randomNumber)
+        {
+            Console.Write("What's your guess? ");
+            guess = int.Parse(Console.ReadLine());
+
+            if (randomNumber > guess)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (randomNumber < guess)
+            {
+                Console.WriteLine("Lower");
+            }
+            else
+            {
+                Console.WriteLine("Congratulations! You guessed it!");
+            }
+
+        }                    
     }
 }
